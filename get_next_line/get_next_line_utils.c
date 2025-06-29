f59803dacb3bce1faa2a426bb1608b53026a239f
jution113 <jution113@gmail.com>
2025-06-29 15:30:46 +0900

    feat: add get next line func

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,70 @@
+#include "get_next_line.h"
+
+char	*init_buffer(char *buffer)
+{
+	if (!buffer)
+	{
+		buffer = malloc(1);
+		if (!buffer)
+			return (NULL);
+		buffer[0] = '\0';
+	}
+	return (buffer);
+}
+
+char	*free_buffer(char *buffer)
+{
+	free(buffer);
+	return (NULL);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	len1;
+	size_t	len2;
+	char	*res;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
+	if (res == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len1)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (j < len2)
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
+}
